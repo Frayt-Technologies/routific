f0402ff19fe570900772e92e8ce992e98205a9c9
Silas <silas@frayt.com>
2020-09-30 14:27:15 -0400

    Update mix.exs
    Bump up version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Routific.MixProject do
   def project do
     [
       app: :routific,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
